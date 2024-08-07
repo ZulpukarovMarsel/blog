c8a35460d340b44dad5333eb9cfd3c1bb312011f
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-08-07 12:12:52 +0600

    added apps verbose name

--- apps/user/apps.py
+++ apps/user/apps.py
@@ -4,7 +4,8 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.user'
+    verbose_name = "Пользователь"
 
     def ready(self):
         import apps.user.signals
-        
+    
